5aa1f759f6d3c591c6e5698fa686962ec6aa103e
srilaxmic <csrilaxmi154@gmail.com>
2019-01-05 17:17:53 +0530

    added pipeline.txt

---
+++ src/main/java/com/geekbull/maven/jenkins2/App.java
@@ -0,0 +1,13 @@
+package com.geekbull.maven.jenkins2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
